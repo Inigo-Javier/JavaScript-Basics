658d405df1193b25867b88ef8578f7d47b2f0408
Iñigo Malluguiza <malluguizamae@gmail.com>
2022-07-08 14:15:53 +0200

    javascript basicos

--- js/modulos.js
+++ js/modulos.js
@@ -1,12 +1,12 @@
 import clasePueba, { PI, usuario, hello, saludar } from "./constantes.js"
-import { aritmetica } from "./aritmetica.js"
+import { aritmetica as calculos } from "./aritmetica.js"
 
 
 
 
 console.log("Archivo modulos.js--------------------------------", PI, usuario)
 
-console.log(aritmetica.sumar(1, 2))
+console.log(calculos.sumar(1, 2))
 hello()
 saludar()
 let pruebaClase = new clasePueba()
