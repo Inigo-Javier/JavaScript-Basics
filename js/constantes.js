def7df1fea0baccf49fb8edfe8a975fae6241f82
Iñigo Malluguiza <malluguizamae@gmail.com>
2022-07-08 12:26:33 +0200

    javascript basicos

---
+++ js/constantes.js
@@ -0,0 +1,9 @@
+export const PI = Math.PI
+
+export const usuario = "Jordan"
+
+
+export default function saludar() {
+    console.log("Hola Módulos ES6")
+}
+// cuando se llame esta funcion saludar se muestra en consola directamente.Sólo se puede utilizar una vez
--- js/modulos.js
+++ js/modulos.js
@@ -1,1 +1,13 @@
-console.log("Archivo modulos.js--------------------------------")
+import { PI, usuario } from "./constantes.js"
+import { aritmetica } from "./aritmetica.js"
+
+
+
+
+console.log("Archivo modulos.js--------------------------------", PI, usuario)
+
+console.log(aritmetica.sumar(1, 2), aritmetica.restar(1, 2))
+
+
+
+
