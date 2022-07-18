69704e8d62de884e918f6a55c3e43daa93136d0e
Iñigo Malluguiza <malluguizamae@gmail.com>
2022-07-18 19:05:20 +0200

    javascript basicos

---
+++ asincronia/async-await.js
@@ -0,0 +1,85 @@
+function cuadradoPromise(value) {
+    if (typeof value !== "number") {
+        return Promise.reject(`Error, el valor "${value}" ingresado no es un número`)
+    }
+
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({
+                value,
+                result: value * value
+            })
+        }, 0 | Math.random() * 1000);
+    })
+}
+
+
+async function funcionAsincronaDeclarada() {
+    try {
+        console.log("Inicio Async Function")
+
+        let obj = await cuadradoPromise(0)//espera al resultado cuadradoPromise(0)
+        console.log(`Async Function ${obj.value} ${obj.result}`)
+
+        obj = await cuadradoPromise(1)
+        console.log(`Async Function ${obj.value} ${obj.result}`)
+
+        obj = await cuadradoPromise(2)
+        console.log(`Async Function ${obj.value} ${obj.result}`)
+
+        obj = await cuadradoPromise(3)
+        console.log(`Async Function ${obj.value} ${obj.result}`)
+
+        obj = await cuadradoPromise("4")
+        console.log(`Async Function ${obj.value} ${obj.result}`)
+
+        obj = await cuadradoPromise(5)
+        console.log(`Async Function ${obj.value} ${obj.result}`)
+
+        obj = await cuadradoPromise(6)
+        console.log(`Async Function ${obj.value} ${obj.result}`)
+
+        console.log("Fin Async Function")
+
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+funcionAsincronaDeclarada()
+
+
+
+const funcionAsincronaExpresasd = async () => {
+    try {
+        console.log("Inicio Async Function")
+
+        let obj = await cuadradoPromise(7)//espera al resultado cuadradoPromise(0)
+        console.log(`Async Function ${obj.value} ${obj.result}`)
+
+        obj = await cuadradoPromise(8)
+        console.log(`Async Function ${obj.value} ${obj.result}`)
+
+        obj = await cuadradoPromise(9)
+        console.log(`Async Function ${obj.value} ${obj.result}`)
+
+        obj = await cuadradoPromise("10")
+        console.log(`Async Function ${obj.value} ${obj.result}`)
+
+        obj = await cuadradoPromise(11)
+        console.log(`Async Function ${obj.value} ${obj.result}`)
+
+        obj = await cuadradoPromise(12)
+        console.log(`Async Function ${obj.value} ${obj.result}`)
+
+        obj = await cuadradoPromise(13)
+        console.log(`Async Function ${obj.value} ${obj.result}`)
+
+        console.log("Fin Async Function")
+
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+funcionAsincronaExpresasd()
--- asincronia/promesas.js
+++ asincronia/promesas.js
@@ -1,48 +1,48 @@
-function cuadradoPromise(value) {
-    if (typeof value !== "number") {
-        return Promise.reject(`Error, el valor "${value}" ingresado no es un número`)
-    }
+// function cuadradoPromise(value) {
+//     if (typeof value !== "number") {
+//         return Promise.reject(`Error, el valor "${value}" ingresado no es un número`)
+//     }
 
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve({
-                value,
-                result: value * value
-            })
-        }, 0 | Math.random() * 1000);
-    })
-}
-//Métodos resolve y reject son estáticos.No necesitan crear una instancia para poder utilizarlos ().
-//NOTA: el setTimeout nos permite simular una petición a un recursos externo y manejar la asincronía.
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve({
+//                 value,
+//                 result: value * value
+//             })
+//         }, 0 | Math.random() * 1000);
+//     })
+// }
+// //Métodos resolve y reject son estáticos.No necesitan crear una instancia para poder utilizarlos ().
+// //NOTA: el setTimeout nos permite simular una petición a un recursos externo y manejar la asincronía.
 
-cuadradoPromise(0)
-    .then(obj => {
-        console.log("inicio Promise")
-        console.log(`${obj.value} ${obj.result}`)
-        return cuadradoPromise(1)
-    })
-    .then(obj => {
-        console.log(`${obj.value} ${obj.result}`)
-        return cuadradoPromise(2)
-    })
-    .then(obj => {
-        console.log(`${obj.value} ${obj.result}`)
-        return cuadradoPromise("3") //simulando error.
-    })
-    .then(obj => {
-        console.log(`${obj.value} ${obj.result}`)
-        return cuadradoPromise(4)
-    })
-    .then(obj => {
-        console.log(`${obj.value} ${obj.result}`)
-        return cuadradoPromise(5)
-    })
-    .then(obj => {
-        console.log(`${obj.value} ${obj.result}`)
-        return cuadradoPromise(6)
-    })
-    .then(obj => {
-        console.log(`${obj.value} ${obj.result}`)
-        console.log("Fin Promise")
-    })
-    .catch(err => console.error(err))
+// cuadradoPromise(0)
+//     .then(obj => {
+//         console.log("inicio Promise")
+//         console.log(`${obj.value} ${obj.result}`)
+//         return cuadradoPromise(1)
+//     })
+//     .then(obj => {
+//         console.log(`${obj.value} ${obj.result}`)
+//         return cuadradoPromise(2)
+//     })
+//     .then(obj => {
+//         console.log(`${obj.value} ${obj.result}`)
+//         return cuadradoPromise("3") //simulando error.
+//     })
+//     .then(obj => {
+//         console.log(`${obj.value} ${obj.result}`)
+//         return cuadradoPromise(4)
+//     })
+//     .then(obj => {
+//         console.log(`${obj.value} ${obj.result}`)
+//         return cuadradoPromise(5)
+//     })
+//     .then(obj => {
+//         console.log(`${obj.value} ${obj.result}`)
+//         return cuadradoPromise(6)
+//     })
+//     .then(obj => {
+//         console.log(`${obj.value} ${obj.result}`)
+//         console.log("Fin Promise")
+//     })
+//     .catch(err => console.error(err))
