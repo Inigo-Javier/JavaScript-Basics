984b03c105751fad12a011665d0ab81118f5dc91
Iñigo Malluguiza <malluguizamae@gmail.com>
2022-07-22 19:36:41 +0200

    javascript basicos

--- POO/poo.js
+++ POO/poo.js
@@ -1,4 +1,4 @@
-// abastraccion,encapsulamiento,herenciay polimorfismo
+
 
 let content = document.getElementById('root')
 
@@ -10,5 +10,47 @@ ulElement.innerHTML = `<li>Abstraccion</li>
                         <li>Herencia</li>
                         <li>Polimorfismo</li>`
 
-console.log(content)
-console.log(ulElement)
+// console.log(content)
+// console.log(ulElement)
+
+
+// Clase: no retorna nada,sólo contiene variables y funciones.
+
+
+class Book {
+    #price
+    constructor(a, b, c) {
+        this.title = a
+        this.author = b
+        this.#price = c
+    }
+    //permite acceder(solo lectura) a la variable privada 'price' 
+    get price() {
+        return this.#price
+    }
+    //permite modificar la variable privada
+    set price(new_price) {
+        this.#price = new_price
+    }
+
+
+}
+
+
+const book = new Book('1989', 'G.O', 350)
+console.log(book)
+
+book.price = 0
+
+console.log(book.price)
+
+
+
+
+
+
+// console.log(book.private)
+
+
+
+
